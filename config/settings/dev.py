b0da1332003b8bbe5e0b58e90177e9d26d54bd90
hsinlei <hsinlei.w@gmail.com>
2020-03-09 06:38:44 +0000

    deploying to aws

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,18 +3,20 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5+f#!xn=hj^u#=cr9@pz@@5cf7bqf0ymy=8uyfpx_zvxpght3='
 
 ADMINS = (
+    ('Charlene Wang', 'hsinlei.w@gmail.com'),
     ('Michael Yin', 'admin@michaelyin.info'),
+    # ('Charlene Wang', 'hsinlei.w@gmail.com'),
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['54.234.209.28', 'localhost', '*']
 
 try:
     from .local import *
--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,26 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
+import os 
+
+env = os.environ.copy()
+#SECRET_KEY = env['SECRET_KEY']
+#SECRET_KEY = 'ababbababa'
+SECRET_KEY = '5+f#!xn=hj^u#=cr9@pz@@5cf7bqf0ymy=8uyfpx_zvxpght3='
+
+
 DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ADMINS = (
+    ('Charlene Wang', 'hsinlei.w@gmail.com'),
+)
+
+
+ALLOWED_HOSTS = ['54.234.209.28']
+
+STATIC_URL = 'http://54.234.209.28:8888/'
 
 try:
     from .local import *
